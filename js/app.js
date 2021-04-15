918a3da5e8d4fa19c99d04462e3a237041d08b51
DESKTOP-9A7E856\pphyo <pphyoe126@gmail.com>
2021-04-15 17:24:08 +0630

    update

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(function(){
+    new WOW().init();
+})
+
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.members-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
